8209562768ed2eecd9d887199173bce354708866
SALAH BKZ <117542403+salahbkz44@users.noreply.github.com>
2025-03-08 11:08:36 +0100

    added player name changer

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function Player() {
+  const enteredPlayeName = useRef();
+  const [playerName, setPlayerName] = useState("unknown entity");
+
+  function handleClick() {
+    setPlayerName(enteredPlayeName.current.value);
+    enteredPlayeName.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={enteredPlayeName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
